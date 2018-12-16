509fbe7ffd2ed650af65b2ec6d2191738ce05319
juandav <e.juandav@gmail.com>
2018-12-15 22:14:54 -0500

    env prod

--- server.js
+++ server.js
@@ -1,27 +1,10 @@
 const jsonServer = require('json-server')
+var express = require('express')
 const server = jsonServer.create()
 const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
 
-router.db._.mixin({
-  getById(collection, id) {
-    const idProp = this.__id()
-    if (Array.isArray(id)) {
-      const ids = id.map(_id => _id.toString())
-      return this.filter(collection, doc => {
-        if (this.has(doc, idProp)) {
-          return ids.includes(doc[idProp].toString())
-        }
-      })
-    }
-    return this.find(collection, doc => {
-      if (this.has(doc, idProp)) {
-        return doc[idProp].toString() === id.toString()
-      }
-    })
-  }
-})
-
+server.use(express.static('build'))
 server.use(middlewares)
 server.use(router)
-server.listen(3000, () =>  console.log('\{^_^}/ hi!'))
+server.listen(9001, () =>  console.log('\{^_^}/ hi!'))
